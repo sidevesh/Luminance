ed65b5c4d87fb98111cb85641aa554fa8224e2f6
Swapnil Devesh <me@sidevesh.com>
2023-07-01 12:16:03 +0530

    Build file fixes

--- build-debug.sh
+++ build-debug.sh
@@ -1,2 +1,2 @@
-#!/bin/sh
+#!/bin/bash
 gcc -g -pthread `pkg-config --cflags gtk+-3.0` -o build/app.debug src/main.c `pkg-config --libs gtk+-3.0` -l ddcutil
--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
-#!/bin/sh
+#!/bin/bash
 gcc -pthread `pkg-config --cflags gtk+-3.0` -o build/app src/main.c `pkg-config --libs gtk+-3.0` -l ddcutil
