8f66ddd6712d2f6a2b536586ea9890939e75e9c7
Swapnil Devesh <me@sidevesh.com>
2024-06-01 06:23:55 +0530

    Update info.c

--- info.c
+++ info.c
@@ -7,4 +7,4 @@
 #define APP_INFO_SOURCE_REPOSITORY_LINK "https://github.com/sidevesh/Luminance"
 #define APP_INFO_LICENSE GTK_LICENSE_GPL_3_0
 #define APP_INFO_PROJECT_AUTHORS "Swapnil Devesh <me@sidevesh.com>", "Ahad Shabbir <shabbirahadh@gmail.com>"
-#define APP_INFO_PROJECT_ARTISTS "Swapnil Devesh <me@sidevesh.com>"
+#define APP_INFO_PROJECT_ARTISTS "Swapnil Devesh <me@sidevesh.com>", "Jakub Steiner <jimmac@gmail.com>"
