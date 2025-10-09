9a1d14df6e2c0b40396dac3e0976a9182b0c95ac
Swapnil Devesh <me@sidevesh.com>
2025-10-10 02:35:02 +0530

    Update OSD command to show all brightness indicators

--- src/osd/providers/experimental/gnome.c
+++ src/osd/providers/experimental/gnome.c
@@ -4,7 +4,7 @@ void show_gnome_osd(gdouble brightness_percentage) {
 	gchar command[300];
 	sprintf(
 		command,
-		"gdbus call --session --dest org.gnome.Shell --object-path /dev/ramottamado/EvalGjs --method dev.ramottamado.EvalGjs.Eval \"Main.osdWindowManager.show(-1, Gio.Icon.new_for_string('display-brightness-symbolic'), null, %f, 1);\" > /dev/null",
+		"gdbus call --session --dest org.gnome.Shell --object-path /dev/ramottamado/EvalGjs --method dev.ramottamado.EvalGjs.Eval \"Main.osdWindowManager.showAll(Gio.Icon.new_for_string('display-brightness-symbolic'), null, %f, 1);\" > /dev/null",
 		brightness_percentage / 100.0
 	);
 	system(command);
